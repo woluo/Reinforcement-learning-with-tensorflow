1fe1b854d0fa90924458afabe539271fddb93812
woluo <fjjth98@163.com>
2018-12-02 17:15:15 +0800

    添加了一点注释，同样改变了epsilon的定义

--- contents/2_Q_Learning_maze/RL_brain.py
+++ contents/2_Q_Learning_maze/RL_brain.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 """
 This part of code is the Q learning brain, which is a brain of the agent.
 All decisions are made in here.
@@ -10,19 +13,25 @@ import pandas as pd
 
 
 class QLearningTable:
-    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+
+    # 接受行为，学习率，收益下降率和epsilon max中的参数为初始值
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.1):
         self.actions = actions  # a list
         self.lr = learning_rate
         self.gamma = reward_decay
         self.epsilon = e_greedy
         self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
 
+    # 接受当前的观测值，选取行为
     def choose_action(self, observation):
         self.check_state_exist(observation)
         # action selection
-        if np.random.uniform() < self.epsilon:
+        if np.random.uniform() < 1 - self.epsilon:
             # choose best action
-            state_action = self.q_table.loc[observation, :]
+            state_action = self.q_table.loc[observation]
+
+            # 这里采用这种写法是因为所有取最大值的行为应该有相同的概率被抽取到
+            # 采用idxmax无法达到这种要求
             # some actions may have the same value, randomly choose on in these actions
             action = np.random.choice(state_action[state_action == np.max(state_action)].index)
         else:
@@ -30,15 +39,17 @@ class QLearningTable:
             action = np.random.choice(self.actions)
         return action
 
+    # 根据一次的观测值，更新Q-table
     def learn(self, s, a, r, s_):
         self.check_state_exist(s_)
         q_predict = self.q_table.loc[s, a]
         if s_ != 'terminal':
-            q_target = r + self.gamma * self.q_table.loc[s_, :].max()  # next state is not terminal
+            q_target = r + self.gamma * self.q_table.loc[s_].max()  # next state is not terminal
         else:
             q_target = r  # next state is terminal
         self.q_table.loc[s, a] += self.lr * (q_target - q_predict)  # update
 
+    # 如果遇到了一个新状态，那么就将之添加到Q-table中（注意：Action本身不会发生变化）
     def check_state_exist(self, state):
         if state not in self.q_table.index:
             # append new state to q table
